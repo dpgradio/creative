3746d07d3ce4a2daf94968fd8417fc39a8a87253
BramSchrooyen <bram.schrooyen@persgroep.net>
2021-07-05 15:57:19 +0200

    add apiClient for quick axios connection to q api

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 import {Q} from './q-sock';
 import hybrid from './utils/hybrid'
+import axios from "axios";
 
 let Sock = Q;
 
-export {Sock, hybrid};
+function apiClient(baseURL) {
+  const apiInstance = axios.create({
+    baseURL
+  });
+
+  apiInstance.interceptors.request.use(config => {
+    if (config.appAuth) {
+      const {radioToken, signature_date, uid, uid_signature} = config.appAuth
+      if (radioToken) {
+        config.headers.Authorization = `Bearer ${radioToken}`
+      } else if (uid) {
+        config.params = {uid, signature_date, uid_signature, ...config.params}
+      } else {
+        throw new Error("No currentUserToken available")
+      }
+    }
+  
+    return config
+  })
+
+  return apiInstance;
+}
+
+export {Sock, hybrid, apiClient};
 
 
 
