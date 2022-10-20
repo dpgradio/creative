d8a4babfbf8025f860f190e90191ef7c0bf6fd95
Siebe Vander Henst <siebe.vander.henst@dpgmedia.be>
2022-10-20 14:10:03 +0200

    refactor: wait until app is loaded and virtualPageView is ready

--- utils/dataLayer.js
+++ utils/dataLayer.js
@@ -8,53 +8,67 @@ let virtualPageParams = {
   platform: hybrid.appInfo.platform
 }
 
+let VirtualPageViewReadyResolve = null;
+let AppLoadedResolve = null;
+
+const VirtualPageViewReadyPromise = new Promise((resolve) => VirtualPageViewReadyResolve = resolve)
+const AppLoadedPromise = new Promise((resolve) => AppLoadedResolve = resolve)
+
 function setVirtualPageViewParams(params) {
   virtualPageParams = {
     ...virtualPageParams,
     ...params
   }
-
-  if (hybrid.appInfo.platform === 'browser') {
-    pushVirtualPageViewEvent()
-  }
 }
 
-function pushVirtualPageViewEvent() {
+function setVirtualPageViewReady() {
+  VirtualPageViewReadyResolve()
+}
+
+Promise.all([VirtualPageViewReadyPromise, AppLoadedPromise], () => {
   window.dataLayer.push({
     event: "VirtualPageView",
     virtualPageURL: virtualPageParams
   });
+});
+
+if (hybrid.appInfo.platform === 'browser') {
+  AppLoadedResolve()
+} else {
+  hybrid.on("appLoad", ({radioToken}) => {
+    if (radioToken) {
+      const {uid} = jwtDecode(radioToken);
+
+      setVirtualPageViewParams({
+        user: {
+          account_id: uid,
+          loggedIn: true
+        }
+      })
+    }
+
+    AppLoadedResolve()
+  })
+
+
+  hybrid.on("authenticated", ({radioToken}) => {
+    if (radioToken) {
+      const {uid} = jwtDecode(radioToken);
+
+      window.dataLayer.push({
+        event: "account_id",
+        user: {
+          account_id: uid,
+          loggedIn: true
+        }
+      });
+    }
+  });
 }
 
-hybrid.on("appLoad", ({ radioToken }) => {
-  if (radioToken) {
-    const { uid } = jwtDecode(radioToken);
 
-    setVirtualPageViewParams({
-      user: {
-        account_id: uid,
-        loggedIn: true
-      }
-    })
-  }
-
-  pushVirtualPageViewEvent()
-});
-
-hybrid.on("authenticated", ({ radioToken }) => {
-  if (radioToken) {
-    const { uid } = jwtDecode(radioToken);
-
-    window.dataLayer.push({
-      event: "account_id",
-      user: {
-        account_id: uid,
-        loggedIn: true
-      }
-    });
-  }
-});
 
 export default {
-  setVirtualPageViewParams
+  setVirtualPageViewParams,
+  setVirtualPageViewReady
 }
