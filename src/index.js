8539078fec4d5e621358c70b66ec6f860cead2de
Casper Boone <mail@casperboone.nl>
2023-02-01 16:57:11 +0100

    Add tap export

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ export { default as hybrid } from './app/hybrid'
 export { default as privacy } from './privacy/privacy'
 export { default as dataLayer } from './privacy/dataLayer'
 
-export { default as loadScript } from './utils/loadScript'
-export { default as openLink } from './utils/openLink'
-
 export { default as socket } from './socket/socket'
 
 export { default as configuration, config } from './config/config'
 
 export { default as api } from './api/api'
+
+export { default as loadScript } from './utils/loadScript'
+export { default as openLink } from './utils/openLink'
+export { default as tap } from './utils/tap'
