bed02069531802ce03d09ab8537ecb893a9a9f9c
Casper Boone <mail@casperboone.nl>
2023-02-16 14:32:02 +0100

    Return null if endpoint is not given

--- src/utils/cdnUrl.js
+++ src/utils/cdnUrl.js
@@ -1,6 +1,9 @@
 const baseUrl = 'https://cdn-radio.dpgmedia.net'
 
 export const cdnImageUrl = (endpoint, size = 'w480') => {
+  if (!endpoint) {
+    return null
+  }
   return `${baseUrl}/site/${size}/${endpoint}`
 }
 
