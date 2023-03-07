ebc6c1541b6ec29a590efdfbac02fbb8275bc194
Siebe Vander Henst <siebe.vander.henst@dpgmedia.be>
2023-03-07 11:22:21 +0100

    add Members endpoints to update phone number

--- src/api/endpoints/Members.js
+++ src/api/endpoints/Members.js
@@ -4,4 +4,8 @@ export default class Members extends Endpoint {
   async me() {
     return await this.api.request().get('/members/me')
   }
+
+  async updatePhoneNumber(mobile) {
+    return await this.api.request().put("/members/me", { profile: { mobile: mobile } })
+  }
 }
