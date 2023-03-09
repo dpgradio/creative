ea8c230e9d63700143eca9870aa7838f5728156e
Siebe Vander Henst <siebe.vander.henst@dpgmedia.be>
2023-03-09 17:20:38 +0100

    refactor rename detection to detections

--- src/api/endpoints/ForbiddenWord.js
+++ src/api/endpoints/ForbiddenWord.js
@@ -2,6 +2,6 @@ import Endpoint from './Endpoint.js'
 
 export default class ForbiddenWord extends Endpoint {
   async detection(id) {
-    await this.api.request().post(`/forbidden_word/${id}/detection`)
+    await this.api.request().post(`/forbidden_word/${id}/detections`)
   }
 }
