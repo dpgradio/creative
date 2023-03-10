9c9b5a1f7845744087e08d633ff1598243068b41
Siebe Vander Henst <siebe.vander.henst@dpgmedia.be>
2023-03-10 11:10:36 +0100

    bugfix forbiddenWord endpoint

--- src/api/endpoints/ForbiddenWord.js
+++ src/api/endpoints/ForbiddenWord.js
@@ -2,6 +2,6 @@ import Endpoint from './Endpoint.js'
 
 export default class ForbiddenWord extends Endpoint {
   async detection(id) {
-    await this.api.request().post(`/forbidden_word/${id}/detections`)
+    await this.api.request().post(`/forbidden_words/${id}/detections`)
   }
 }
