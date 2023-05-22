48303439e46583064a8a0f50223beab8b4e44a19
Siebe Vander Henst <siebe.vander.henst@persgroep.net>
2023-05-22 13:58:21 +0200

    change to country code

--- src/utils/mobile.js
+++ src/utils/mobile.js
@@ -1,8 +1,18 @@
 import { config } from '../config/config'
 
-export const removeCountryPrefix = (mobile, localPhoneNumberPrefix = config('phone_number_prefix')) => {
-  if (localPhoneNumberPrefix) {
-    mobile = mobile.replace(`${localPhoneNumberPrefix}.`, '0')
+export const removeCountryPrefix = (mobile, prefix) => {
+  if (!prefix) {
+    switch (config('country_code')) {
+      case 'BE':
+        prefix = '+32'
+        break
+      case 'NL':
+        prefix = '+31'
+        break
+    }
+  }
+  if (prefix) {
+    mobile = mobile.replace(`${prefix}.`, '0')
   }
   mobile = mobile.replace('.', '')
 
