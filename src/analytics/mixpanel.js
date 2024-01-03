08ebdc62a716147965dbe906c58d4d8bb9dbb09e
Siebe Vander Henst <siebe.vander.henst@persgroep.net>
2024-01-03 09:38:54 +0100

    add mixpanel

---
+++ src/analytics/mixpanel.js
@@ -0,0 +1,35 @@
+import { privacy } from '../privacy/privacy'
+
+class Mixpanel {
+  constructor() {
+    this.trackingEvents = false
+    this.trackBacklog = []
+    this.mixpanel = null
+  }
+
+  initialize(Mixpanel, { mixpanelId, track_pageview = true }) {
+    this.mixpanel = Mixpanel
+    privacy.push('analytics', () => {
+      this.mixpanel.init(mixpanelId, {
+        track_pageview,
+        persistence: 'localStorage',
+        api_host: 'https://api-eu.mixpanel.com',
+      })
+
+      while (this.trackBacklog.length > 0) {
+        const { eventName, properties } = this.trackBacklog.shift()
+        this.mixpanel.track(eventName, properties)
+      }
+    })
+  }
+
+  trackEvent(eventName, properties) {
+    if (this.trackingEvents) {
+      this.mixpanel.track(eventName, properties)
+    } else {
+      this.trackBacklog.push({ eventName, properties })
+    }
+  }
+}
+
+export default new Mixpanel()
