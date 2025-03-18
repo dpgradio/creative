758e0e25b4f7e8e699d457235522716a8e0fbec1
Jan De Poorter <jan@defv.be>
2025-03-18 10:56:08 +0100

    Create native event listeners ahead of time
    This might be a reason of some Android crashes

--- src/app/hybrid.js
+++ src/app/hybrid.js
@@ -24,6 +24,19 @@ class Hybrid {
     // Hook this on window so it can be required in multiple packs
     window._hybridEventSubscriptions = window._hybridEventSubscriptions || {}
 
+    // Ensure these native events don't error out because nothing is listening
+    for (const event of [
+      'appLoad',
+      'audioStateChanged',
+      'authenticated',
+      'didAppear',
+      'didHide',
+      'didPrompt',
+      'didAlert',
+    ]) {
+      this.ensureTriggerExists(event)
+    }
+
     this._cachedRadioTokenOnLoad = undefined
     this.on('appLoad', (context) => {
       this._cachedRadioTokenOnLoad = context?.radioToken ?? null
