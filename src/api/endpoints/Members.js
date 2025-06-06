b8f7ff6e1e6c1b086382edebf15c6126e6ee2944
pjaspers <piet@pjaspers.com>
2025-06-06 16:54:40 +0200

    Add the basic info endpoint

--- src/api/endpoints/Members.js
+++ src/api/endpoints/Members.js
@@ -5,6 +5,10 @@ export default class Members extends Endpoint {
     return await this.api.request().get('/members/me')
   }
 
+  async basicInfo(profileId, email) {
+    return await this.api.request().get(`/members/basic-info/${profileId}`, {email: email})
+  }
+
   async updateProfile(profile) {
     return await this.api.request().put('/members/me', { profile })
   }
